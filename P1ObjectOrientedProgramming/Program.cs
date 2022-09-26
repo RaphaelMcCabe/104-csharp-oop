05645c3728e707d818df50156b458b5834c2ba82
RaphaelMcCabe <112477763+RaphaelMcCabe@users.noreply.github.com>
2022-09-26 14:50:35 +0200

    Assignment P1ObjectOrientedProgramming

---
+++ P1ObjectOrientedProgramming/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string[] names = new string[3];
+
+for (int i = 0; i < names.Length; i++)
+{
+    Console.WriteLine("Give me a name");
+    names[i] = Console.ReadLine();
+}
+Console.WriteLine($"{names[0]},{names[1]},{names[2]}");
