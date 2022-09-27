8f2bb4232d84cff014092144fbf32ca2a7cb46be
RaphaelMcCabe <112477763+RaphaelMcCabe@users.noreply.github.com>
2022-09-27 11:36:47 +0200

    Assignment P2Classes

---
+++ P2Classes/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Class person = new Class();
+Class animal = new Class();
+Class car = new Class();
+
+person.name = "Person";
+animal.name = "Animal";
+car.name = "Car";
+
+person.Call();
+animal.Call();
+car.Call();
+public class Class
+{
+    public string name;
+
+    public void Call()
+    {
+        Console.WriteLine(name);
+    }
+    }
