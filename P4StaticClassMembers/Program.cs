5cee1757d36e8ea0f527603970c760d2ba4d218f
RaphaelMcCabe <112477763+RaphaelMcCabe@users.noreply.github.com>
2022-09-28 14:18:14 +0200

    Assignment P4StaticClassMembers

---
+++ P4StaticClassMembers/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+Console.WriteLine(Supermath.Lerp(0f, 100f, 0.1f));
+
+
+public static class Supermath
+{
+    public static float Lerp(float from, float to, float t)
+    {
+        return from + (to - from) * t;
+    }
+
+}
