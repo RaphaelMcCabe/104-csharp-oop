5ef3300067d740cdde1c32838377d01fe55f3396
RaphaelMcCabe <112477763+RaphaelMcCabe@users.noreply.github.com>
2022-10-06 10:18:15 +0200

    RPG Exercise

---
+++ RPG/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+Unit unit = new Unit("Skeleton");
+Unit unit2 = new Unit("Demon");
+Unit unit3 = new Unit("Ghost");
+unit.ReportStatus();
+unit2.ReportStatus();
+unit3.ReportStatus();
+
+public class Unit
+{
+    public int id;
+    public static int nextID;
+    string name;
+    public Unit(string name)
+    {
+        this.name = name;
+        id = nextID;
+        nextID++;
+    }
+    
+    public void ReportStatus()
+    {
+        Console.WriteLine($"{id}# {name}");
+        
+        return;
+    }
+    
+    
+    
+    
+}
+
+
