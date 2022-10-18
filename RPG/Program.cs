3fc9a287b84e45bbf96b1d97a0f3cdf4f4519a2f
RaphaelMcCabe <112477763+RaphaelMcCabe@users.noreply.github.com>
2022-10-18 15:03:08 +0200

    9.1 Inheritance

--- RPG/Program.cs
+++ RPG/Program.cs
@@ -1,9 +1,44 @@
 ﻿Unit unit = new Unit("Skeleton", 100);
 Unit unit2 = new Unit("Demon", 100);
 Unit unit3 = new Unit("Ghost",100);
+Unit unit4 = new Unit("Necromancer",200);
 unit.ReportStatus();
 unit2.ReportStatus();
 unit3.ReportStatus();
+unit4.ReportStatus();
+
+while (unit4.Health > 0)
+{
+    Console.WriteLine("How much damage do you want to deal to the Necromancer");
+    unit4.Damage(Convert.ToInt32(Console.ReadLine()));
+    unit4.ReportStatus();
+}
+
+public class Necromancer : Unit
+{
+    private bool hasRessurected;
+    public void Ressurect()
+    {
+        if (Health <= 0)
+        {
+            
+        }
+    }
+    
+    public Necromancer(string name, int maxHealth) : base(name, maxHealth)
+    {
+        
+    }
+    public override void Damage(int value)
+    {
+        base.Damage(value);
+        if (Health < 0 && !hasRessurected)
+        {
+            Ressurect();
+        }
+    }
+}
+
 
 public class Unit
 {
@@ -21,23 +56,21 @@ public class Unit
         this.maxHealth = maxHealth;
         health = maxHealth;
     }
-    
-
     public int Health
     {
-        set
+        private set
         {
             if (value < 0)
             {
                 health = 0;
             }
 
-            if (value > maxHealth)
+            if (value >= maxHealth)
             {
                 health = maxHealth;
             }
 
-            if (value > 0 && value < maxHealth)
+            if (value >= 0 && value <= maxHealth)
             {
                 health = value;
             }
@@ -48,9 +81,14 @@ public class Unit
         }
     }
     
+    
     public void ReportStatus()
     {
         Console.WriteLine($"{id}# {name} {Health}/{maxHealth}");
     }
-    
+
+    public virtual void Damage(int value)
+    {
+        Health -= value;
+    }
 }
