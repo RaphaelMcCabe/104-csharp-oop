7cab2f2f5f8fd445e0a7f8331204a758d51168d2
RaphaelMcCabe <112477763+RaphaelMcCabe@users.noreply.github.com>
2022-10-18 13:49:16 +0200

    8.3 Properties

--- RPG/Program.cs
+++ RPG/Program.cs
@@ -1,29 +1,56 @@
-﻿
-Unit unit = new Unit("Skeleton");
-Unit unit2 = new Unit("Demon");
-Unit unit3 = new Unit("Ghost");
+﻿Unit unit = new Unit("Skeleton", 100);
+Unit unit2 = new Unit("Demon", 100);
+Unit unit3 = new Unit("Ghost",100);
 unit.ReportStatus();
 unit2.ReportStatus();
 unit3.ReportStatus();
 
-
 public class Unit
 {
+    private int maxHealth;
+    private int health;
     public int id;
     public static int nextID;
     string name;
-    public Unit(string name)
+   
+    public Unit(string name, int maxHealth)
     {
         this.name = name;
         id = nextID;
         nextID++;
+        this.maxHealth = maxHealth;
+        health = maxHealth;
+    }
+    
+
+    public int Health
+    {
+        set
+        {
+            if (value < 0)
+            {
+                health = 0;
+            }
+
+            if (value > maxHealth)
+            {
+                health = maxHealth;
+            }
+
+            if (value > 0 && value < maxHealth)
+            {
+                health = value;
+            }
+        }
+        get
+        {
+            return health;
+        }
     }
     
     public void ReportStatus()
     {
-        Console.WriteLine($"{id}# {name}");
-        
-        return;
+        Console.WriteLine($"{id}# {name} {Health}/{maxHealth}");
     }
     
 }
